10682c7174b3ebaddc0d5213b4ee6e5e1410a337
mario32111 <marioge44@gmail.com>
2025-04-07 11:34:47 -0600

    proyecto antes del examen

---
+++ ProyectoTBD/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+
+namespace ProyectoTBD
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+            con = new SqlConnection(
+                 "Server =localhost;" +
+                 "Initial Catalog=Tienda;" +
+                 $"User= {user}; Password = {pass};"+
+                 "Trust server certificate = True");
+
+            try
+            {
+                con.Open();
+                MessageBox.Show("Inicio de sesion exitoso");
+                Form form1 = new Form1();
+                form1.Show();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error al iniciar sesion...."+ ex.Message);
+            }
+        }
+    }
+}
